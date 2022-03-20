1e603a56f5981a7f0b77c197f84223c2a3a7bc64
Zhangzhhome <94028527+Zhangzhhome@users.noreply.github.com>
2022-03-20 20:00:36 +0800

    searchBar的搜索小逻辑

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,63 +1,80 @@
-// components/SearchBar/SearchBar.js
 // Component({
-//     /**
-//      * 组件的属性列表
-//      */
 //     properties: {
-
+//       placeholder: {
+//         type: String,
+//         value: '',
+//       }
 //     },
-
-//     /**
-//      * 组件的初始数据
-//      */
 //     data: {
-
+//       inputValue: ''
 //     },
-
-//     /**
-//      * 组件的方法列表
-//      */
 //     methods: {
-
+//       // 用户输入触发
+//       handleInput: function(e) {
+//         this.setData({
+//           inputValue: e.detail.value
+//         })
+//       },
+//       // 点击清空输入框icon
+//       handleDeleteClick: function() {
+//         this.setData({
+//           inputValue: ''
+//         })
+//       },
+//       // 点击取消触发
+//       handleTextbtnClick() {
+//         // 触发父组件中的方法
+//         this.setData({
+//           inputValue: ''
+//         })
+//       },
+//       // 用户点击确定触发
+//       handleConfirm() {
+//         this.triggerEvent('handleSearch', this.data.inputValue)
+//         console.log(this.data)
+//       },
 //     }
-// })
 
-Component({
-    properties: {
-      placeholder: {
-        type: String,
-        value: '',
-      }
-    },
-    data: {
-      inputValue: ''
-    },
-    methods: {
-      // 用户输入触发
-      handleInput: function(e) {
-        this.setData({
-          inputValue: e.detail.value
-        })
-      },
-      // 点击清空输入框icon
-      handleDeleteClick: function() {
-        this.setData({
-          inputValue: ''
-        })
-      },
-      // 点击取消触发
-      handleTextbtnClick() {
-        // 触发父组件中的方法
-        this.setData({
-          inputValue: ''
-        })
-      },
-      // 用户点击确定触发
-      handleConfirm() {
-        this.triggerEvent('handleSearch', this.data.inputValue)
-        console.log(this.data)
-      },
+
+//   })
+
+Page({
+  data: {
+    inputShowed: false,
+    inputVal: ''
+  },
+  showInput() {
+    this.setData({
+      inputShowed: true,
+    });
+  },
+  hideInput() {
+    this.setData({
+      inputVal: '',
+      inputShowed: false,
+    });
+  },
+  clearInput() {
+    this.setData({
+      inputVal: '',
+    });
+  },
+  inputTyping(e) {
+    this.setData({
+      inputVal: e.detail.value,
+    });
+  },
+  inputTyping(e){
+    this.setData({
+      inputVal: e.detail.value
+    })
+  },
+  search(){
+    var flag = this.data.inputVal;
+    if(flag){
+      wx.switchTab({
+        url: '/pages/search/search',
+      })
     }
-
-
-  })
+ }
+});
