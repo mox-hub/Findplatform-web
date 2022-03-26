6ce20e8dffbaa31e938b37db1f4a7302935b44ad
Zhangzhhome <94028527+Zhangzhhome@users.noreply.github.com>
2022-03-26 12:05:39 +0800

    图片上传成功跳转

--- pages/item/item_upload.js
+++ pages/item/item_upload.js
@@ -123,6 +123,11 @@ Page({
           current: e.currentTarget.id, // 当前显示图片的http链接
           urls: this.data.files, // 需要预览的图片http链接列表
         });
+      },
+      itemSuccess(){
+        wx.navigateTo({
+          url: '../msg/msg_success',
+        })
       }
 });
 
--- pages/msg/msg_success.js
+++ pages/msg/msg_success.js
@@ -12,7 +12,11 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+          setTimeout(() => {
+            wx.navigateTo({
+              url: '../item/item_details',
+            })
+          }, 3000);
     },
 
     /**
