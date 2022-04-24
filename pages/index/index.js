35bf27909daf7a2a0fea852182d003ed6efe4b2c
Mox <key10240@163.com>
2022-04-24 23:02:16 +0800

    大范围更改weui为colorUI【未完成】

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
-//Page Object
+// pages/index/index.js
+const app = getApp();
 Page({
   data: {
     searchBtnState:false
@@ -6,3 +7,40 @@ Page({
   onLoad: function(options){
   },
 });
+
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+    list: [{
+        title: '我是拾物者',
+        background: 'bg-green-gradient',
+        img: '/images/index/icons8-emoji1-64.png',
+        url: '/item/item_upload'
+    },
+      {
+        title: '我是失物者',
+        background: 'bg-red-gradient',
+        img: '/images/index/icons8-emoji-64.png',
+        url: '/search/search'
+      }
+    ]
+  },
+  methods: {
+    toChild(e) {
+      if (e.currentTarget.dataset.url == "/search/search") {
+        wx.switchTab({
+          url: '/pages' + e.currentTarget.dataset.url
+        })
+      }
+      else {
+        wx.navigateTo({
+          url: '/pages' + e.currentTarget.dataset.url
+        })
+      }
+    },
+  }
+})
